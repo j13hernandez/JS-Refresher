80a41f74009a9f427881af350146724118c4c8bd
Palma <1hernandezj3@gmail.com>
2022-04-01 23:04:09 -0400

    "Basic arrow functions "

---
+++ play.js
@@ -0,0 +1,19 @@
+let name = 'Juli';
+let age = 27;
+let hasHobbies = true;
+
+const summerizeUser = (userName, userAge, userHasHobbies) => {
+  return `Name is ${userName}, age is ${userAge}, and user has hobbies: ${userHasHobbies}`;
+};
+
+const add = (a, b) => a + b;
+
+const addOne = (a) => a + 1;
+
+const addRand = () => 11 + 2;
+
+console.log(addOne(2));
+
+console.log(add(1, 2));
+
+console.log(summerizeUser(name, age, hasHobbies));

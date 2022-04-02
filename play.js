68a395a6c9a2398def48c5f3a0b1dd2e0eaf98a5
Palma <1hernandezj3@gmail.com>
2022-04-02 00:12:43 -0400

    "Destructoring objects and arrays"

--- play.js
+++ play.js
@@ -1,3 +1,11 @@
+// let name = 'Juli';
+// let age = 27;
+// let hasHobbies = true;
+
+// const summerizeUser = (userName, userAge, userHasHobbies) => {
+//   return `Name is ${userName}, age is ${userAge}, and user has hobbies: ${userHasHobbies}`;
+// };
+
 const person = {
   name: 'juli',
   age: 27,
@@ -6,13 +14,25 @@ const person = {
   },
 };
 
-let name = 'Juli';
-let age = 27;
-let hasHobbies = true;
+const { name } = person;
+console.log(name);
+const copiedPerson = { ...person };
 
-const summerizeUser = (userName, userAge, userHasHobbies) => {
-  return `Name is ${userName}, age is ${userAge}, and user has hobbies: ${userHasHobbies}`;
-};
+const hobbies = ['Fitness', 'Coding'];
+
+console.log(hobbies.map((hobby) => `Hobby: ${hobby}`));
+console.log(hobbies);
+
+hobbies.push('Swimming');
+console.log(hobbies);
+
+const copiedHobby = [...hobbies];
+
+const [hobby1] = copiedHobby;
+console.log(hobby1);
+
+console.log(copiedPerson);
+console.log(copiedHobby);
 
 const add = (a, b) => a + b;
 
@@ -24,6 +44,6 @@ console.log(addOne(2));
 
 console.log(add(1, 2));
 
-console.log(summerizeUser(name, age, hasHobbies));
+// console.log(summerizeUser(name, age, hasHobbies));
 
 person.greet();
